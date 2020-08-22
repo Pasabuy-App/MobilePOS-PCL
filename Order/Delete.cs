a5cd8b44b667bdf2c8824798b2bafb4d46232649
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-22 14:58:57 +0800

    Added Delete Order.

---
+++ Order/Delete.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+using MobilePOS.Order.Struck;
+
+namespace MobilePOS.Order
+{
+    public class Delete
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Delete Order Class.
+        /// </summary>
+        private static Delete instance;
+        public static Delete Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Delete();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public Delete()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void Order(string wp_id, string session_key, string odid, string pid, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("odid", odid);
+            dict.Add("pid", pid);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/mobilepos/v1/customer/order/delete", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
