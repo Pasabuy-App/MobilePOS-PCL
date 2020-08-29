d056cc4848ec872ba613bc03ae3b3f9c8dd822a3
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-29 14:36:23 +0800

    Added Store Class and deleted store folder.

---
+++ Controller/Store.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using MobilePOS.Controller.Struct;
+
+
+namespace MobilePOS.Controller
+{
+    public class Store
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Store Class with process and total sales method.
+        /// </summary>
+        private static Store instance;
+        public static Store Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Store();
+                return instance;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public Store()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+
+        #region Process Method
+        public async void Process(string wp_id, string session_key, string stid, string odid, string stage, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("stid", stid);
+            dict.Add("odid", odid);
+            dict.Add("stage", stage);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/mobilepos/v1/store/order/process", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+
+        #region TotalSales Method
+        public async void TotalSales(string wp_id, string session_key, string stid, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("stid", stid);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/mobilepos/v1/store/total/sales", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
