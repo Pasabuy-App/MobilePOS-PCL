deee16857581953d3adc2ab021d2e4397584726c
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-29 14:33:18 +0800

    Deleted Operations folder and added Operations Class.

---
+++ Controller/Operation.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using MobilePOS.Controller.Struct;
+
+namespace MobilePOS.Controller
+{
+    public class Operation
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Operations Class with listing by date, by month and open stores method.
+        /// </summary>
+        private static Operation instance;
+        public static Operation Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Operation();
+                return instance;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public Operation()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+
+        #region ListByDate Method
+        public async void ListByDate(string wp_id, string session_key, string start, string end, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("start", start);
+            dict.Add("end", end);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/mobilepos/v1//operation/listing/bydate", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+
+        #region ListByMonth Method
+        public async void ListByMonth(string wp_id, string session_key, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/mobilepos/v1/operation/listing/bymonth", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+
+        #region ListOpenStore Method
+        public async void ListOpenStore(string wp_id, string session_key, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/mobilepos/v1/operation/listing/openstore", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
--- Controller/Struct/Token.cs
+++ Controller/Struct/Token.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MobilePOS.Order.Struct
+namespace MobilePOS.Controller.Struct
 {
     public class Token
     {
